b8df3c54fac6401bd2b6cb44fccf37568eef6e34
James Carman <jcarman@apache.org>
2014-03-26 14:17:39 -0400

    Adding stopping test.

--- java/src/test/java/com/carmanconsulting/akka/StoppingTest.java
+++ java/src/test/java/com/carmanconsulting/akka/StoppingTest.java
@@ -1,6 +1,9 @@
 package com.carmanconsulting.akka;
 
 import akka.actor.ActorRef;
+import akka.actor.PoisonPill;
+import akka.actor.Props;
+import akka.actor.Terminated;
 import akka.actor.UntypedActor;
 import org.junit.Test;
 
@@ -10,22 +13,40 @@ public class StoppingTest extends AkkaTestCase {
 //----------------------------------------------------------------------------------------------------------------------
 
     @Test
-    public void testStoppingWithSystem() {
-        ActorRef ref = system().actorOf(HelloAkka.props(), "hello");
+    public void testStopMethod() {
+        ActorRef echo = system().actorOf(Props.create(EchoAndStop.class), "echo");
+        watch(echo);
+        echo.tell("Hello", testActor());
+        expectMsg("Hello");
+        expectMsgClass(Terminated.class);
     }
 
 //----------------------------------------------------------------------------------------------------------------------
 // Inner Classes
 //----------------------------------------------------------------------------------------------------------------------
 
-    public static class MyActor extends UntypedActor {
+    public static class EchoAndStop extends UntypedActor {
         @Override
         public void onReceive(Object message) throws Exception {
-            if ("Done".equals(message)) {
-                context().stop(self());
-            } else {
-                unhandled(message);
+            sender().tell(message, self());
+            context().stop(self());
+        }
+    }
+
+    public static class EchoAndStopAfterN extends UntypedActor {
+        private int n;
+
+        public EchoAndStopAfterN(int n) {
+            this.n = n;
+        }
+
+        @Override
+        public void onReceive(Object message) throws Exception {
+            sender().tell(message, self());
+            if (n-- == 0) {
+                self().tell(PoisonPill.getInstance(), ActorRef.noSender());
             }
+
         }
     }
 }
